6c9b5efa07618dfc57d60c85fadcf2435cdbd2ae
Jorge Barrientos <jorge.barrientos@hulu.com>
2019-08-05 16:23:37 -0500

    Adding initialization for onOpen callback on Room

--- app/src/main/java/com/example/archcomponentscodelab/db/WordRoomDatabase.kt
+++ app/src/main/java/com/example/archcomponentscodelab/db/WordRoomDatabase.kt
@@ -4,8 +4,12 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.archcomponentscodelab.dao.WordDao
 import com.example.archcomponentscodelab.models.Word
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 /**
  * When you modify the database schema, you'll need to update the version number and define a migration strategy
@@ -24,7 +28,7 @@ abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context, scope: CoroutineScope): WordRoomDatabase {
             val tempInstance = INSTANCE
             if (tempInstance != null) {
                 return tempInstance
@@ -34,10 +38,33 @@ abstract class WordRoomDatabase : RoomDatabase() {
                     context.applicationContext,
                     WordRoomDatabase::class.java,
                     "Word_database"
-                ).build()
+                ).addCallback(WordDatabaseCallback(scope)).build()
                 INSTANCE = instance
                 return instance
             }
+        }
+    }
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onOpen(db: SupportSQLiteDatabase) {
+            super.onOpen(db)
+            INSTANCE?.let { database ->
+                scope.launch(Dispatchers.IO) {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            wordDao.deleteAll()
+
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
         }
     }
 }
