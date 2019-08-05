aee33853439ff272acdc08ff1fac5990234f1126
Jorge Barrientos <jorge.barrientos@hulu.com>
2019-08-05 16:15:38 -0500

    Creating needed resources to display word list

--- app/src/main/java/com/example/archcomponentscodelab/MainActivity.kt
+++ app/src/main/java/com/example/archcomponentscodelab/MainActivity.kt
@@ -5,6 +5,9 @@ import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu
 import android.view.MenuItem
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.archcomponentscodelab.adapter.WordListAdapter
 
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,11 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
+        initRecyclerView()
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+    }
+
+    private fun initRecyclerView() {
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
+        val adapter = WordListAdapter(this)
+        recyclerView.adapter = adapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
