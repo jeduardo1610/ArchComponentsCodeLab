a37ba2114b2cb0e43a31c4fae7784f780865a9af
Jorge Barrientos <jorge.barrientos@hulu.com>
2019-08-05 16:06:56 -0500

    Some comments added

--- app/src/main/java/com/example/archcomponentscodelab/viewmodel/WordViewModel.kt
+++ app/src/main/java/com/example/archcomponentscodelab/viewmodel/WordViewModel.kt
@@ -25,3 +25,15 @@ class WordViewModel(application: Application) : AndroidViewModel(application) {
         repository.insert(word)
     }
 }
+/*
+Warning: A memory leak can occur if you keep in the ViewModel a reference to a context with a shorter lifecycle:
+
+Activity
+Fragment
+View
+All these objects can be destroyed by the operative system and recreated when there's a configuration change, and this can happen many times during the lifecycle of a ViewModel.
+
+As an example, if you keep a reference of an Activity in the ViewModel, you may end up with a reference to a destroyed Activity. This is a memory leak.
+
+If you need the application context, use AndroidViewModel, as shown in this codelab.
+ */
